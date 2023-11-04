b497d7fc09bd430e8757a7a4436807d5087080b9
Érick Augusto Paludo <100646545+ErickPaludo@users.noreply.github.com>
2023-11-04 16:42:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-function teste(){
+function teste() {
 
-        var mudar = document.querySelector("#area");
-        var top = Math.floor(Math.random() * (400 - (100) + 1)) + (250);
-        var left = Math.floor(Math.random() * (250 - (500) + 1)) + (450);
-    
-        mudar.style.position = "absolute"; 
-        mudar.style.top = top + "px"; 
-        mudar.style.left = left + "px"; 
-    }
-    function Sim(){
-        var sim = document.querySelector("#resposta");
-        sim.innerHTML = "Eu sabia! kkk"
-    }
+    var mudar = document.querySelector("#area");
+    var top, left;
+    var area = mudar.getBoundingClientRect(); // Obtém as dimensões do elemento "area"
+
+    // Loop para garantir que a nova posição não se sobrepõe ao elemento "area"
+    do {
+        top = Math.floor(Math.random() * (800 - 600 + 1)) + 250;
+        left = Math.floor(Math.random() * (550 - 1000 + 1)) + 450;
+    } while (top + area.height > area.top && top < area.bottom && left + area.width > area.left && left < area.right);
+
+    mudar.style.position = "absolute";
+    mudar.style.top = top + "px";
+    mudar.style.left = left + "px";
+}
+
+function Sim() {
+    var imagem = document.querySelector("#imagem");
+    imagem.src = "download.jpg"
+}
