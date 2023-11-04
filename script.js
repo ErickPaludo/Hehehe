b7f3bad28963dc717c0a84320ae005d40e25e1e3
Érick Augusto Paludo <100646545+ErickPaludo@users.noreply.github.com>
2023-11-04 16:26:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function teste(){
 
-        var mudar = document.querySelector("#op");
-        var top = Math.floor(Math.random() * (200 - (100) + 1)) + (250);
-        var left = Math.floor(Math.random() * (200 - (100) + 1)) + (150);
+        var mudar = document.querySelector("#area");
+        var top = Math.floor(Math.random() * (400 - (100) + 1)) + (250);
+        var left = Math.floor(Math.random() * (250 - (500) + 1)) + (450);
     
         mudar.style.position = "absolute"; 
         mudar.style.top = top + "px"; 
